b66aa26ef033a16a36c9dd4dd6fd6fe3b0fc112a
christine.scala22@gmail.com <christine.scala22@gmail.com>
2020-07-14 21:11:26 -0400

    conditional

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var bestnum = 22;
+/*var bestnum = 22;
 
 var worstnum = "word";
 
@@ -15,3 +15,27 @@ var total= trick+number;
 console.log (total);
 /* true + 2       
 interprets true as 1 and returns 3*/
+
+var counter = 2;
+var answer = counter+5;
+var mult = answer*2;
+//divide 14 by 2 output remainder
+var mode = mult%3;
+
+console.log (mode);
+
+if(jelly % 2 == 0) {
+    console.log("Even");
+ } else {
+    console.log("Odd");
+ }
+
+ var time = 8;
+
+if (time < 7 || time > 12) {
+  console.log ("Go back to sleep");
+} else if (time > 7 && time < 8 ) {
+ console.log ("Hit snooze");
+} else {
+  console.log ("Time to get up");
+}
