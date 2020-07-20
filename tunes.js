9460368b9cf9f3884110e2e4b9aae71db07b2fbe
christine.scala22@gmail.com <christine.scala22@gmail.com>
2020-07-19 23:20:22 -0400

    ques 4

--- tunes.js
+++ tunes.js
@@ -6,9 +6,25 @@
 function jukebox() {
     var songChoice = document.getElementById("request").value;
 
-
 if (songChoice !== null || songChoice !== "") {
     document.getElementById("request").style.border = "solid 1px green";
     document.getElementById("song").innerHTML = songChoice;
 }
+}
+
+var tunes = ["Piano man,", "Meant to Be,", "Bang!"];
+
+/* was tring to get what the user typed be aded to the string.
+tunes.push(song);
+document.getElementById("music").innerHTML;
+*/
+
+var lullabye = 
+    document.getElementById("music").innerHTML;
+
+for (var i=0; i < tunes.length; i++){
+  console.log(lullabye);
+  lullabye = lullabye + "\n" + tunes[i];
+  
+  document.getElementById("music").innerHTML = lullabye;
 }
