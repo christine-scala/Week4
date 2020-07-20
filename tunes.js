130ced316506fd8f3f47c3d184aa1642c991ecb3
christine.scala22@gmail.com <christine.scala22@gmail.com>
2020-07-20 11:54:54 -0400

    adding to array

--- tunes.js
+++ tunes.js
@@ -1,30 +1,25 @@
-/* 
- create a function that gets called when we click the button
- going to create a globally scoped array. Right now it should be empty
- */
+//create empty array
+tunes = [];
 
 function jukebox() {
+// grab text (value) from input id=request
     var songChoice = document.getElementById("request").value;
 
+// create for loop saying as long as input is not empty
 if (songChoice !== null || songChoice !== "") {
-    document.getElementById("request").style.border = "solid 1px green";
-    document.getElementById("song").innerHTML = songChoice;
-}
-}
-
-var tunes = ["Piano man,", "Meant to Be,", "Bang!"];
-
-/* was tring to get what the user typed be aded to the string.
-tunes.push(song);
-document.getElementById("music").innerHTML;
-*/
-
-var lullabye = 
-    document.getElementById("music").innerHTML;
-
-for (var i=0; i < tunes.length; i++){
-  console.log(lullabye);
-  lullabye = lullabye + "\n" + tunes[i];
   
-  document.getElementById("music").innerHTML = lullabye;
+//put a green border around box if input is not empty
+    document.getElementById("request").style.border = "solid 1px green";
+//add the value of the requested song (now nicknamed my var songChoice) into my array 
+ tunes.push(songChoice);
+//make an empty storage container to hold requests
+ var content = "";
+ //start a loop - add contents of storage container to array 
+  for (var i=0; i < tunes.length; i++){
+   content += tunes[i]; 
+    content += " ";
+  }
+//send the contents of storage container out to screen
+document.getElementById("song").innerHTML = content
+}
 }
